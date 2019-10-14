432e9565ae48447ffbccd5e45fc10ea4a73c0392
jewdore <jerry.dong@upltv.com>
2019-10-14 14:28:17 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
 # aws-es-proxy-by-ec2
+
+本项目提供一个简单的HTTP代理，让用户可以在本地直接访问线上的Amazon Elasticsearch service。
+使用本项目，用户需要提供EC2的ssh权限相关信息。本项目通过ssh执行远程命令，获取EC2的meta data。
+并根据获取的meta data 加密用户的代理请求。
+
+
+Fork from [aws-es-proxy](https://github.com/abutaha/aws-es-proxy)
+
+改动原因：
+
+**aws es proxy**必须提供AWS_ACCESS_KEY_ID、AWS_SECRET_ACCESS_KEY相关标识信息，但生产机器常常是不允许本地的链接，即没有key和token相关信息，也不允许IAM roles策略访问。
+
+改动：
+
+用户提供登录机器EC2的ssh权限。
+本项目会通过ssh 执行远程命令获取EC2的meta data，然后设置ENV。从而允许用户通过本地直接访问线上封闭的ES
+
+## Installation
+
+### Download binary executable
+
+**aws-es-proxy** has single executable binaries for Linux, Mac and Windows.
+
+Download the latest [aws-es-proxy release](https://github.com/abutaha/aws-es-proxy/releases/).
+
+### Docker
+
+There is an official docker image avaiable for aws-es-proxy. To run the image:
+
+```sh
+# v0.9 and newer (latest always point to the latest release):
+
+docker run --rm -it abutaha/aws-es-proxy:0.9 -endpoint https://dummy-host.ap-southeast-2.es.amazonaws.com
+
+v.08:
+
+docker run --rm -it abutaha/aws-es-proxy ./aws-es-proxy -endpoint https://dummy-host.ap-southeast-2.es.amazonaws.com
+
+```
+### Via homebrew
+
+```sh
+brew install aws-es-proxy
+```
+## Configuring Credentials
+
+在使用 **aws-es-proxy-by-ec2** 之前, 请设置允许ES访问的EC2的ssh权限配置信息, 类似:
+
+```
+server: 'EC2 IP'
+port: '22'
+user: 'EC2 USERNAME'
+key: "
+EC2 PRIVATE KEY (或通过password)
+"
+timeout: 120
+proxy:
+   server: '代理IP'
+   port: '22'
+   user: '代理用户'
+   password: '代理用户密码'
+   timeout: 120
+```
+
+改配置是直接提供给[easyssh-proxy库](https://github.com/appleboy/easyssh-proxy)使用，请参考改go module
+
+## Usage example:
+
+```sh
+./aws-es-proxy -endpoint https://test-es-somerandomvalue.eu-west-1.es.amazonaws.com
+Listening on 127.0.0.1:9200
+```
+
+*aws-es-proxy-by-ec2* listens on 127.0.0.1:9200 ， 你可以通过传参数 `-listen`更改。
+
+```sh
+./aws-es-proxy -listen :8080 -endpoint ...
+./aws-es-proxy -listen 10.0.0.1:9200 -endpoint ...
+```
+
+使用 `-h` 可以获取到命令行help:
+
+```sh
+./aws-es-proxy -h
+Usage of ./aws-es-proxy:
+  -endpoint string
+        Amazon ElasticSearch Endpoint (e.g: https://dummy-host.eu-west-1.es.amazonaws.com)
+  -listen string
+        Local TCP port to listen on (default "127.0.0.1:9200")
+  -log-to-file
+        Log user requests and ElasticSearch responses to files
+  -no-sign-reqs
+        Disable AWS Signature v4
+  -pretty
+        Prettify verbose and file output
+  -ssh-config string
+        Access to ec2 machine information
+  -verbose
+        Print user requests
+```
+
---
+++ aws-es-proxy.go
@@ -0,0 +1,449 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/appleboy/easyssh-proxy"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+	"gopkg.in/yaml.v2"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"path"
+	"regexp"
+	"strings"
+	"time"
+
+	// log "github.com/Sirupsen/logrus"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws/signer/v4"
+	uuid "github.com/satori/go.uuid"
+)
+
+type requestStruct struct {
+	Requestid  string
+	Datetime   string
+	Remoteaddr string
+	Requesturi string
+	Method     string
+	Statuscode int
+	Elapsed    float64
+	Body       string
+}
+
+type responseStruct struct {
+	Requestid string
+	Body      string
+}
+
+type proxy struct {
+	scheme       string
+	host         string
+	region       string
+	service      string
+	endpoint     string
+	verbose      bool
+	prettify     bool
+	logtofile    bool
+	nosignreq    bool
+	fileRequest  *os.File
+	fileResponse *os.File
+	credentials  *credentials.Credentials
+	ssh *easyssh.MakeConfig
+}
+
+type metaData struct {
+	AccessKeyId  string `json:"AccessKeyId"`
+	SecretAccessKey string `json:"SecretAccessKey"`
+	Token string `json:"Token"`
+}
+
+func newProxy(args ...interface{}) *proxy {
+	return &proxy{
+		endpoint:  args[0].(string),
+		verbose:   args[1].(bool),
+		prettify:  args[2].(bool),
+		logtofile: args[3].(bool),
+		nosignreq: args[4].(bool),
+	}
+}
+
+func (p *proxy) parseEndpoint() error {
+	var link *url.URL
+	var err error
+
+	if link, err = url.Parse(p.endpoint); err != nil {
+		return fmt.Errorf("error: failure while parsing endpoint: %s. Error: %s",
+			p.endpoint, err.Error())
+	}
+
+	// Only http/https are supported schemes
+	switch link.Scheme {
+	case "http", "https":
+	default:
+		link.Scheme = "https"
+	}
+
+	// Unknown schemes sometimes result in empty host value
+	if link.Host == "" {
+		return fmt.Errorf("error: empty host or protocol information in submitted endpoint (%s)",
+			p.endpoint)
+	}
+
+	// AWS SignV4 enabled, extract required parts for signing process
+	if !p.nosignreq {
+		// Extract region and service from link
+		parts := strings.Split(link.Host, ".")
+
+		if len(parts) == 5 {
+			p.region, p.service = parts[1], parts[2]
+		} else {
+			//return fmt.Errorf("error: submitted endpoint is not a valid Amazon ElasticSearch Endpoint")
+		}
+	}
+
+	// Update proxy struct
+	p.scheme = link.Scheme
+	p.host = link.Host
+
+	return nil
+}
+
+func (p *proxy)Run(cmd string, timeout time.Duration) (outStr string) {
+	outStr, _, _, err := p.ssh.Run(cmd, timeout)
+	if err != nil {
+		log.Fatalln("error while getMetaData. Error: ", err.Error())
+	}
+	return outStr
+}
+
+func (p *proxy)getCredentialsFromEC2()  {
+	stdout, stderr, done, err := p.ssh.Run("curl http://169.254.169.254/latest/meta-data/iam/security-credentials/",120 * time.Second)
+	if err != nil {
+		panic("Can't run remote command: " + err.Error())
+	} else {
+		fmt.Println("don is :", done, "stdout is :", stdout, ";   stderr is :", stderr)
+	}
+	roleName := p.Run("curl http://169.254.169.254/latest/meta-data/iam/security-credentials/",120 * time.Second)
+	metaDataStr := p.Run("curl http://169.254.169.254/latest/meta-data/iam/security-credentials/" + roleName,120 * time.Second)
+    log.Println(metaDataStr)
+    var metaDataStruct = &metaData{}
+	json.Unmarshal([]byte(metaDataStr),metaDataStruct)
+	os.Setenv("AWS_ACCESS_KEY_ID",metaDataStruct.AccessKeyId)
+	os.Setenv("AWS_SECRET_ACCESS_KEY",metaDataStruct.SecretAccessKey)
+	os.Setenv("AWS_SESSION_TOKEN",metaDataStruct.Token)
+}
+
+func (p *proxy) getSigner() *v4.Signer {
+	// Refresh credentials after expiration. Required for STS
+	endpointResolver, cleanupFn := setupCredentialsEndpoints()
+	defer cleanupFn()
+	if p.credentials == nil {
+		p.getCredentialsFromEC2()
+		sess := session.Must(session.NewSessionWithOptions(session.Options{
+			Config: aws.Config{
+				EndpointResolver: endpointResolver,
+			},
+		}))
+		credentials := sess.Config.Credentials
+		p.credentials = credentials
+		log.Println("Generated fresh AWS Credentials object")
+	}
+	return v4.NewSigner(p.credentials)
+}
+
+func (p *proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path,"r.url.path")
+	if r.URL.Path == "/meta-data/iam/security-credentials/RoleName" {
+		w.Write([]byte(ec2MetadataResponse))
+		return
+	} else if r.URL.Path == "/meta-data/iam/security-credentials/" {
+		w.Write([]byte("RoleName"))
+		return
+	}else if r.URL.Path == "/favicon.ico" {
+		w.Write([]byte("RoleName"))
+		return
+	}
+
+	requestStarted := time.Now()
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		log.Fatalln("error while dumping request. Error: ", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
+
+	ep := *r.URL
+	ep.Host = p.host
+	ep.Scheme = p.scheme
+	ep.Path = path.Clean(ep.Path)
+
+	req, err := http.NewRequest(r.Method, ep.String(), r.Body)
+	if err != nil {
+		log.Fatalln("error creating new request. ", err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	addHeaders(r.Header, req.Header)
+
+	// Make signV4 optional
+	if !p.nosignreq {
+		// Start AWS session from ENV, Shared Creds or EC2Role
+		signer := p.getSigner()
+
+		// Sign the request with AWSv4
+		payload := bytes.NewReader(replaceBody(req))
+		signer.Sign(req, payload, p.service, p.region, time.Now())
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalln(err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if !p.nosignreq {
+		// AWS credentials expired, need to generate fresh ones
+		if resp.StatusCode == 403 {
+			p.credentials = nil
+			return
+		}
+	}
+
+	defer resp.Body.Close()
+
+	// Write back headers to requesting client
+	copyHeaders(w.Header(), resp.Header)
+
+	// Send response back to requesting client
+	body := bytes.Buffer{}
+	if _, err := io.Copy(&body, resp.Body); err != nil {
+		log.Fatalln(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.WriteHeader(resp.StatusCode)
+	w.Write(body.Bytes())
+
+	requestEnded := time.Since(requestStarted)
+
+	/*############################
+	## Logging
+	############################*/
+
+	rawQuery := string(dump)
+	rawQuery = strings.Replace(rawQuery, "\n", " ", -1)
+	regex, _ := regexp.Compile("{.*}")
+	regEx, _ := regexp.Compile("_msearch|_bulk")
+	queryEx := regEx.FindString(rawQuery)
+
+	var query string
+
+	if len(queryEx) == 0 {
+		query = regex.FindString(rawQuery)
+	} else {
+		query = ""
+	}
+
+	if p.verbose {
+		if p.prettify {
+			var prettyBody bytes.Buffer
+			json.Indent(&prettyBody, []byte(query), "", "  ")
+			t := time.Now()
+
+			fmt.Println()
+			fmt.Println("========================")
+			fmt.Println(t.Format("2006/01/02 15:04:05"))
+			fmt.Println("Remote Address: ", r.RemoteAddr)
+			fmt.Println("Request URI: ", ep.RequestURI())
+			fmt.Println("Method: ", r.Method)
+			fmt.Println("Status: ", resp.StatusCode)
+			fmt.Printf("Took: %.3fs\n", requestEnded.Seconds())
+			fmt.Println("Body: ")
+			fmt.Println(string(prettyBody.Bytes()))
+		} else {
+			log.Printf(" -> %s; %s; %s; %s; %d; %.3fs\n",
+				r.Method, r.RemoteAddr,
+				ep.RequestURI(), query,
+				resp.StatusCode, requestEnded.Seconds())
+		}
+	}
+
+	if p.logtofile {
+
+		requestID := uuid.NewV4()
+
+		reqStruct := &requestStruct{
+			Requestid:  requestID.String(),
+			Datetime:   time.Now().Format("2006/01/02 15:04:05"),
+			Remoteaddr: r.RemoteAddr,
+			Requesturi: ep.RequestURI(),
+			Method:     r.Method,
+			Statuscode: resp.StatusCode,
+			Elapsed:    requestEnded.Seconds(),
+			Body:       query,
+		}
+
+		respStruct := &responseStruct{
+			Requestid: requestID.String(),
+			Body:      string(body.Bytes()),
+		}
+
+		y, _ := json.Marshal(reqStruct)
+		z, _ := json.Marshal(respStruct)
+		p.fileRequest.Write(y)
+		p.fileRequest.WriteString("\n")
+		p.fileResponse.Write(z)
+		p.fileResponse.WriteString("\n")
+
+	}
+
+}
+
+// Recent versions of ES/Kibana require
+// "kbn-version" and "content-type: application/json"
+// headers to exist in the request.
+// If missing requests fails.
+func addHeaders(src, dest http.Header) {
+	if val, ok := src["Kbn-Version"]; ok {
+		dest.Add("Kbn-Version", val[0])
+	}
+
+	if val, ok := src["Content-Type"]; ok {
+		dest.Add("Content-Type", val[0])
+	}
+}
+const ec2MetadataResponse = `{
+	  "Code": "Success",
+	  "Type": "AWS-HMAC",
+	  "AccessKeyId" : "ec2-access-key",
+	  "SecretAccessKey" : "ec2-secret-key",
+	  "Token" : "token",
+	  "Expiration" : "2100-01-01T00:00:00Z",
+	  "LastUpdated" : "2009-11-23T0:00:00Z"
+	}`
+func setupCredentialsEndpoints() (endpoints.Resolver, func()) {
+	resolver := endpoints.ResolverFunc(
+		func(service, region string, opts ...func(*endpoints.Options)) (endpoints.ResolvedEndpoint, error) {
+			switch service {
+			case "ec2metadata":
+				return endpoints.ResolvedEndpoint{
+					URL: "127.0.0.1:9200",
+				}, nil
+			default:
+				return endpoints.ResolvedEndpoint{},
+					fmt.Errorf("unknown service endpoint, %s", service)
+			}
+		})
+
+	return resolver, func() {
+
+	}
+}
+
+// Signer.Sign requires a "seekable" body to sum body's sha256
+func replaceBody(req *http.Request) []byte {
+	if req.Body == nil {
+		return []byte{}
+	}
+	payload, _ := ioutil.ReadAll(req.Body)
+	req.Body = ioutil.NopCloser(bytes.NewReader(payload))
+	return payload
+}
+
+func copyHeaders(dst, src http.Header) {
+	for k, vals := range src {
+		for _, v := range vals {
+			dst.Add(k, v)
+		}
+	}
+}
+
+func main() {
+
+	var (
+		verbose       bool
+		prettify      bool
+		logtofile     bool
+		nosignreq     bool
+		endpoint      string
+		listenAddress string
+		sshYamlConfig string
+		fileRequest   *os.File
+		fileResponse  *os.File
+		err           error
+	)
+
+	flag.StringVar(&endpoint, "endpoint", "", "Amazon ElasticSearch Endpoint (e.g: https://dummy-host.eu-west-1.es.amazonaws.com)")
+	flag.StringVar(&listenAddress, "listen", "127.0.0.1:9200", "Local TCP port to listen on")
+	flag.StringVar(&sshYamlConfig,"ssh-config","", "Access to ec2 machine information")
+	flag.BoolVar(&verbose, "verbose", false, "Print user requests")
+	flag.BoolVar(&logtofile, "log-to-file", false, "Log user requests and ElasticSearch responses to files")
+	flag.BoolVar(&prettify, "pretty", false, "Prettify verbose and file output")
+	flag.BoolVar(&nosignreq, "no-sign-reqs", false, "Disable AWS Signature v4")
+	flag.Parse()
+
+	if len(os.Args) < 3 {
+		fmt.Println("You need to specify Amazon ElasticSearch endpoint.")
+		fmt.Println("Please run with '-h' for a list of available arguments.")
+		os.Exit(1)
+	}
+
+	p := newProxy(
+		endpoint,
+		verbose,
+		prettify,
+		logtofile,
+		nosignreq,
+	)
+
+	if err = p.parseEndpoint(); err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
+	}
+
+	if p.logtofile {
+		u1 := uuid.NewV4()
+		u2 := uuid.NewV4()
+		requestFname := fmt.Sprintf("request-%s.log", u1.String())
+		responseFname := fmt.Sprintf("response-%s.log", u2.String())
+
+		if fileRequest, err = os.Create(requestFname); err != nil {
+			log.Fatalln(err.Error())
+		}
+		if fileResponse, err = os.Create(responseFname); err != nil {
+			log.Fatalln(err.Error())
+		}
+
+		defer fileRequest.Close()
+		defer fileResponse.Close()
+
+		p.fileRequest = fileRequest
+		p.fileResponse = fileResponse
+
+	}
+
+	ssh := &easyssh.MakeConfig{}
+	yamlFile, err := ioutil.ReadFile(sshYamlConfig)
+	if err != nil {
+		log.Fatalf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, ssh)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+	p.ssh = ssh
+
+	log.Printf("Listening on %s...\n", listenAddress)
+	log.Fatal(http.ListenAndServe(listenAddress, p))
+}
